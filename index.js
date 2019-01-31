6bd39b6e440fe29254d16d709b143be0ff5e965b
Dani Lyn <dlyn1110@gmail.com>
2019-01-31 18:12:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ function findMatching (collection, name) {
     });
 }
 
-function fuzzyMatch(collection, letter) {
-   return collection.filter(function (driverName) { return driverName.slice(0, letter.length) === letter;
-    });
-}
-
-function matchName(collection, name) {
-    return collection.filter(function (driverName) { return driverName.name === name;
-    })
-}
+// function fuzzyMatch(collection, letter) {
+//    return collection.filter(function (driverName) { return driverName.slice(0, letter.length) === letter;
+//     });
+// }
+// 
+// function matchName(collection, name) {
+//     return collection.filter(function (driverName) { return driverName.name === name;
+//     })
+// }
