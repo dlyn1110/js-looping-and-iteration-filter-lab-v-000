7bd2832ac9a6eb843ac6eaa1f55c4613d098494b
Dani Lyn <dlyn1110@gmail.com>
2019-01-31 18:10:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 
 function findMatching(collection, name) {
-  return collection.filter(function (driverName){
+  return collection.filter(function (driverName) {
     return driverName.toLowerCase() === name.toLowerCase() ;
   });
 }
