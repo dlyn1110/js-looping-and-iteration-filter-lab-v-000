4bdd697a753bf97aa2facc7540caf990287e31b8
Dani Lyn <dlyn1110@gmail.com>
2019-01-31 18:08:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file
+
+function findMatching(collection, name) {
+  return collection.filter(function (driverName){
+    return driverName.toLowerCase() === name.toLowerCase() ;
+  });
+}
+
+function fuzzyMatch
